99b5d0670bac6444cc6d1f94865baa16c662adc7
andyreesecups <andyreesecups@yahoo.com>
2019-03-31 15:20:18 -0500

    set up initial object structure and deserializing the xml file into that object structure

--- XmlTranslator/Program.cs
+++ XmlTranslator/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using XmlTranslator.Models;
 
 namespace XmlTranslator
 {
@@ -10,6 +13,14 @@ namespace XmlTranslator
     {
         static void Main(string[] args)
         {
+                XmlSerializer serializer = new XmlSerializer(typeof(Invoice));
+                Invoice invoice = new Invoice();
+
+                using (FileStream fileStream = new FileStream(@"C:\temp\Non_PO_Invoice.xml", FileMode.Open))
+                {
+                    invoice = (Invoice)serializer.Deserialize(fileStream);
+                }
+
         }
     }
 }
