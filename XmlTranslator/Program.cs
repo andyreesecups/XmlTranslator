e9a651ab6d1a44b5c70b793653e2a9ab870f427f
andyreesecups <andyreesecups@yahoo.com>
2019-05-04 17:34:52 -0500

    if InsertDataToDatabase fails then call XMLErrorMessageWriter function, else call XMLSuccessMessageWriter function

--- XmlTranslator/Program.cs
+++ XmlTranslator/Program.cs
@@ -13,11 +13,17 @@ namespace XmlTranslator
             // Call DeserializeToObject Function
             Data.FileAccess.DeserializeToObject(out invoice);
 
-            // Call XMLSuccessMessageWriter
-            Data.FileAccess.XMLSuccessMessageWriter(invoice);
 
-            // Call XMLErrorMessageWriter
-            Data.FileAccess.XMLErrorMessageWriter(invoice);
+            // If InsertDataToDatabase fails then call XMLErrorMessageWriter to write failed XML Acknowledgment
+            if(Data.DataAccess.InsertDataToDatabase(invoice, out Message) == false)
+            {
+                Data.FileAccess.XMLErrorMessageWriter(invoice);
+            }
+            // Else call XMLSuccessMessageWriter to write success XML Acknowledgment
+            else
+            {
+                Data.FileAccess.XMLSuccessMessageWriter(invoice);
+            }
         }
     }
 }
